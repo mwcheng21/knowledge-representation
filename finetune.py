4057a884ba29b8c241885eec51cf0a0bba93e596
Micah Cheng <heymicahme@gmail.com>
2022-04-08 21:14:59 -0400

    Update eval to bleu score

--- finetune.py
+++ finetune.py
@@ -4,6 +4,8 @@ from transformers import PLBartModel, PLBartConfig
 from transformers import Trainer
 import numpy as np
 from datasets import load_metric
+from nltk.translate.bleu_score import corpus_bleu
+
 
 class Model():
     def __init__(self, pretrained_tokenizer_path=None, pretrained_model_path=None):
@@ -12,32 +14,25 @@ class Model():
         if pretrained_model_path==None:
             self.tokenizer = PLBartTokenizer.from_pretrained("uclanlp/plbart-base")
         else:
-            #TODO: make sure this works
             self.tokenizer = PLBartTokenizer.from_pretrained(pretrained_tokenizer_path, local_files_only=True)
 
-
-        #TODO: what model to use PLBartModel or PLBartForConditionalGeneration?
+        #TODO: what model to use PLBartModel or PLBartForConditionalGeneration? Not sure the difference
         if pretrained_model_path==None:
             self.model = PLBartModel.from_pretrained("uclanlp/plbart-base")
         else:
-            #TODO: load model
-            pass
+            self.model = PLBartModel.from_pretrained(pretrained_model_path, local_files_only=True)
 
-        #use this metric in training
-        self.metric = load_metric("accuracy")
-
-    def preprocess(self, save_dir):
-        '''Loads src code, preprocesses and saves into files in save_dir'''
-        #TODO import dataset
-        self.train_dataset = None
-        self.eval_dataset = None
-        #TODO: look at javalang or some other library to create AST??
-        #TODO: save AST into files so we don't have to preprocess later
+        self.metric = load_metric("bleu")
 
     def load_datasets(self, save_dir):
-        '''Combines 3 modalities into vectors'''
+        '''Load datasets from <save_dir> files (3 modalities)'''
+
+        #Load into a DataLoader
+        self.train_dataset = None
+        self.eval_dataset = None
         pass
         #TODO: append tokenized examples, summary, and context (hstack or something????)
+
 
     def train(self, save_file):
         '''Finetune model using transformers Trainer class. Save final model in <save_file>'''
@@ -51,31 +46,36 @@ class Model():
             compute_metrics=self.compute_metrics,
         )
 
+
         #train it #TODO: we can also write fine tuning by hand using pytorch
         trainer.train()
 
-        #TODO: make sure to save the model
+        self.model.save_pretrained(save_file+"_finetuned.pt")
+        #TODO: Do we need to train tokenizer?
+        self.tokenizer.save_pretrained(save_file+"_tokenizer_finetuned.pt")
 
-        #TODO: process or return output
         return None
 
+    #TODO: do we evaluate bleu_1, 2, 3 and 4? and rouge_L and CIDEr?
     def evaluate(self):
-        '''Evaluate eval set on self.model'''
-        pass
+        '''Evaluate model on test set'''
+        preds = self.model.predict(self.eval_dataset)
+        bleuScore = corpus_bleu(self.eval_dataset.targets, preds)
+        return bleuScore
 
     def compute_metrics(self, eval_pred):
         '''Helper function for what metric training should use'''
         logits, labels = eval_pred
         predictions = np.argmax(logits, axis=-1)
-
-        #TODO: Not sure if this is right, since labels arent exactly the same, (what metric to use for accuracy???,)
         return self.metric.compute(predictions=predictions, references=labels)
 
+    def run(self):
+        '''Run finetuning'''
+        model.load_datasets()
+        model.train("basic_AST")
+        bleuScore = model.evaluate()
+        print("Bleu Score: ", bleuScore)
 
 if __name__ == "__main__":
     model = Model()
-    #TODO: uncomment as needed to test
-    # model.preprocess()
-    # model.load_datasets()
-    # model.train()
-    # model.evaluate()
+    model.run()
