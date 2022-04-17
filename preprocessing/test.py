f2fb22d748d4eaadbce1dc9efa3de4a30d2464ce
leahcimsu <xinhao.su@columbia.edu>
2022-04-17 19:28:07 -0400

    Data Flow edge done

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -1,4 +1,5 @@
-from preprocessing import Traveser
+from preprocessing import Traveser, preprocess
+import os
 
 code = open("test.java", "r").read()
 
@@ -7,9 +8,18 @@ def main():
     visitor = Traveser('java', code)
     visitor.travesal()
 
-
     # print(visitor.nodes)
     # print(visitor.control_flow_nodes)
-    # print(visitor.edges)
+    print(visitor.edges)
 
-main()
+
+def main2():
+    input_file = os.path.join(os.path.dirname(os.getcwd()), 'data/medium/train/data.buggy_only')
+    os.makedirs( os.path.join(os.path.dirname(os.getcwd()), 'encoded-data/medium/train'), exist_ok=True)
+    out_file = os.path.join(os.path.dirname(os.getcwd()), 'encoded-data/medium/train/data.buggy_only')
+    preprocess(input_file, out_file, 'fullGraph')
+
+
+if __name__ == '__main__':
+    main2()
+
