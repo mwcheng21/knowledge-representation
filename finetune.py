8e75ca41fa96ca1a2f1f9a8c64f75b22a43529a8
leahcimsu <xinhao.su@columbia.edu>
2022-04-30 19:00:44 -0400

    Add bacth run

--- finetune.py
+++ finetune.py
@@ -3,21 +3,26 @@ from transformers.trainer_seq2seq import Seq2SeqTrainer
 from transformers import PLBartForConditionalGeneration, PLBartTokenizer
 import numpy as np
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 from itertools import zip_longest
 from datasets import Dataset
-from torch import squeeze
+from torch import squeeze, cuda
+import math
+import GPUtil
+import torch
+GPUtil.showUtilization()
 
 
 class Model():
     def __init__(self, model_name, pretrained_tokenizer_path=None, pretrained_model_path=None):
 
         '''Config'''
-        os.environ["CUDA_VISIBLE_DEVICES"]="1"
+        if(cuda.is_available()): print('GPU in using', cuda.current_device())
 
         '''Init function'''
         self.model_name = model_name
         #init pretrained model and tokenizer
-        if pretrained_model_path==None:
+        if pretrained_tokenizer_path==None:
             self.tokenizer = PLBartTokenizer.from_pretrained("uclanlp/plbart-base", tgt_lang="java")
         else:
             self.tokenizer = PLBartTokenizer.from_pretrained(pretrained_tokenizer_path, local_files_only=True)
@@ -31,8 +36,6 @@ class Model():
         else:
             self.model = PLBartForConditionalGeneration.from_pretrained(pretrained_model_path, local_files_only=True)
     
-        
-
 
     def load_datasets(self, save_dir):
         '''Load datasets from <save_dir> files (3 modalities)'''
@@ -82,6 +85,7 @@ class Model():
             })
         return out["train"], out["eval"], out["test"]
 
+
     def train(self):
         '''Finetune model using transformers Trainer class. Save final model in /models/model_name'''
         #initialize the transformers trainer
@@ -105,9 +109,79 @@ class Model():
 
         self.model.save_pretrained("model/" + self.model_name + "_finetuned.pt")
 
+
+    def get_portion(self, batch_id: int, total_batch_num: int, total_sample_size: int):
+        portion_size = round(total_sample_size / total_batch_num)
+        start = batch_id * portion_size
+        end = start + portion_size
+        return start, end
+
+
+    def batch_train(self, batch_id: int, total_batch_num: int, pretrained_model_path: str) -> str:
+        #initialize the transformers trainer
+        config = {
+            "output_dir": f"training/trainer_{batch_id}",
+            "evaluation_strategy": "epoch",
+            "generation_max_length": 512,
+            # "per_device_train_batch_size": 2,
+            # "per_device_eval_batch_size": 4
+        }
+
+        # Update the model
+        if pretrained_model_path is not None:
+            assert os.path.exists(pretrained_model_path)
+            model = PLBartForConditionalGeneration.from_pretrained(pretrained_model_path, local_files_only=True)
+        else:
+            model = self.model
+
+        train_start, train_end = self.get_portion(batch_id, total_batch_num, len(self.train_dataset))
+
+        eval_start, eval_end = self.get_portion(batch_id, total_batch_num, len(self.eval_dataset))
+  
+        # debug
+        print("Training Portion: ",train_start, train_end)
+
+        print("Eval Portion: ", eval_start, eval_end)
+
+
+        if batch_id < total_batch_num - 1:
+            train_dataset = torch.utils.data.Subset(self.train_dataset, list(range(train_start, train_end, 1)))
+            eval_dataset = torch.utils.data.Subset(self.eval_dataset, list(range(eval_start, eval_end, 1)))
+        else:
+            train_dataset = torch.utils.data.Subset(self.train_dataset, list(range(train_start, len(self.train_dataset), 1)))
+            eval_dataset = torch.utils.data.Subset(self.eval_dataset, list(range(eval_start, len(self.eval_dataset), 1)))
+
+        
+        training_args = Seq2SeqTrainingArguments(**config)
+        trainer = Seq2SeqTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            compute_metrics=self.compute_metrics,
+        )
+        
+        trainer.train()
+
+        checkpoint_path = f"model/{self.model_name}_{batch_id}_finetuned.pt"
+
+        model.save_pretrained(checkpoint_path)  
+
+        if batch_id == total_batch_num - 1:
+            return checkpoint_path, trainer
+        else:
+            return checkpoint_path, None
+
+
     def evaluate(self):
         '''Evaluate model on test set by accuracy'''
         test_pred = self.trainer.predict(self.test_dataset)
+        return self.compute_metrics(test_pred)
+
+
+    def batch_evaluate(self, trainer):
+        '''Evaluate model on test set by accuracy'''
+        test_pred = trainer.predict(self.test_dataset)
         return self.compute_metrics(test_pred)
 
 
@@ -152,8 +226,33 @@ class Model():
         print("Evaluating...")
         acc = model.evaluate()
         print("Accuracy: ", acc)
+
+    
+    def batch_run(self, folder_name: str, batch_num: int = 3):
+        '''Run Batch finetuning'''
+        print(f'''Dataset split uniformly by {batch_num} Portions''')
+        print("Loading datasets...")
+        self.load_datasets(folder_name)
+        batch_id = 0
+        pretrain_path = None
+
+        while batch_id != batch_num:
+            print("Finetuning", batch_id, "Portion")
+            pretrain_path, trainer = self.batch_train(batch_id, batch_num, pretrain_path)
+            batch_id += 1
+            print("Cleaning Cache")
+            cuda.empty_cache()
+
+
+        print("Evaluating...")
+        acc = self.batch_evaluate(trainer)
         
+        print("Accuracy: ", acc)
+    
+
+    
 
 if __name__ == "__main__":
-    model = Model("test")
-    model.run("./data/medium")
+    model = Model("Modit-G", pretrained_model_path="./test_trainer/checkpoint-12000")
+    ## model.batch_run("./data/tiny", 4)
+    model.run("./data/tiny")
